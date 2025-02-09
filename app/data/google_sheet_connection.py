63a5f974ecb13dbfecd1c808c7ca3f4fc66835ff
renanortegax <renan.xavier44@gmai.com>
2025-02-08 21:12:34 -0300

    Criando func append row na planilha

--- app/data/google_sheet_connection.py
+++ app/data/google_sheet_connection.py
@@ -1,12 +1,14 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from typing import List, Any, Dict
-import logging
 import os
+from app.config import log_config
+from datetime import datetime
 
+logger = log_config()
 class GoogleSheetDb:
     """
-    Classe que encapsula a conexão e operações básicas em uma planilha do Google Sheets
+    Classe que encapsula a conexão e operações básicas na planilha do Google Sheets
     """
     def __init__(self, 
                  sheet_name: str, 
@@ -26,7 +28,7 @@ class GoogleSheetDb:
         # Abre a planilha
         self.spreadsheet = self.client.open(sheet_name)
 
-        # Seleciona a worksheet (página) desejada pelo índice (ou nome, se preferir)
+        # Seleciona pelo índice ou pelo nome
         self.worksheet = self.spreadsheet.get_worksheet(worksheet_index)
 
     def get_all_records(self) -> List[Dict[str, Any]]:
@@ -41,16 +43,12 @@ class GoogleSheetDb:
         Adiciona uma linha ao final da planilha
         """
         self.worksheet.append_row(row_data)
-        logging.info(f"Linha adicionada: {row_data}")
+        logger.info(f"Linha adicionada: {row_data}")
 
     def update_cell(self, row: int, col: int, value: Any):
         """
-        Atualiza uma célula específica (row, col).
-        As linhas e colunas são 1-based (não zero-based).
-
-        :param row: Índice da linha (1-based).
-        :param col: Índice da coluna (1-based).
-        :param value: Valor a ser gravado na célula.
+        Atualiza uma célula específica (row, col)
+        As linhas e colunas começam em 1
         """
         self.worksheet.update_cell(row, col, value)
 
@@ -60,3 +58,17 @@ class GoogleSheetDb:
         Retorna o objeto 'Cell' ou levanta gspread.exceptions.CellNotFound se não achar
         """
         return self.worksheet.find(query)
+
+    def create_row_to_append(self, request_data):
+        """ Pega as informações da mensagem recebida e retorna um dicionario """
+        data = {}
+        timestamp_sender = request_data.get('entry')[0].get('changes')[0].get('value').get('messages')[0].get('timestamp')
+        data['id'] = self.get_all_records()[-1].get('id') + 1
+        data['sender_time'] = datetime.fromtimestamp(int(timestamp_sender)).strftime('%Y-%m-%d %H:%M:%S')
+        data['sender_name'] = request_data.get('entry')[0].get('changes')[0].get('value').get('contacts')[0].get('profile').get('name')
+        data['sender_number'] = request_data.get('entry')[0].get('changes')[0].get('value').get('messages')[0].get('from')
+        data['direction'] = 'received'
+        data['message_type'] = request_data.get('entry')[0].get('changes')[0].get('value').get('messages')[0].get('type')
+        data['text'] = request_data.get('entry')[0].get('changes')[0].get('value').get('messages')[0].get('text').get('body')
+        return data
+

5c7b11a9d90fb94f7db973dc1da09c8d3f0e8203
renanortegax <renan.xavier44@gmail.com>
2025-02-16 13:02:33 -0300

    Criacao classe envia mensagem

---
+++ app/utils/message_sender.py
@@ -0,0 +1,56 @@
+import requests
+from app.config import log_config
+from dotenv import load_dotenv
+import os
+from flask import current_app, jsonify
+import json 
+
+logger = log_config("app.utils.message_sender")
+load_dotenv()
+
+class MessageSender:
+    def __init__(self):
+        self.endpoint = "messages"
+        self.phone_numer_id = current_app.config['PHONE_NUMBER_ID']
+        self.headers = {
+            "Content-type": "application/json",
+            "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}",
+        }
+        self.url = f"https://graph.facebook.com/{current_app.config['VERSION']}/{current_app.config['PHONE_NUMBER_ID']}/{self.endpoint}"
+
+        # logger.info('Classe MessageSender inicializada com os parâmetros %s', vars(MessageSender))
+
+    def get_parameters_message_sender(self, phone_number_to, text):
+        return json.dumps(
+            {
+                "messaging_product": "whatsapp",
+                "recipient_type": "individual",
+                "to":phone_number_to,
+                "type": "text",
+                "text": {"preview_url": False, "body": text},
+            }
+        )
+
+    def send_message(self, parameters):
+        url = self.url
+        headers = self.headers
+
+        logger.info("Parametros de chamada: %s", parameters)
+        logger.info("URL: %s", self.url)
+        logger.info("Headers: %s", self.headers)
+        try:
+            response = requests.post(url, data=parameters, headers=headers, timeout=10)
+            response.raise_for_status()
+        
+        except requests.Timeout:
+            logger.error("Timeout enquanto respondia a mensagem")
+            return jsonify({"status":"error", "message":"Timeout"}), 400
+
+        except (requests.RequestException) as e:
+            logger.error("Request error: %s", e)
+            return jsonify({"status":"error", "message":"Failed"}), 500
+        
+        else: # Se passar
+            logger.info("Status: %s", response.status_code)
+            logger.info("Response sent: %s", response.json())
+            return response
